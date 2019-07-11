8b0a4f2e4c194b0c368ff23d24cfd7c4e18088d4
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-07-11 13:38:34 -0400

    made more changes to js, still not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,60 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
 var winCount = 0;
 var lossCount = 0;
 var remainingGuess = 10;
 var theChosenLetter = "";
 var guessedLetter = [];
 
-function() {
+resetGame();
+display();
 
+document.onkeyup = function (event) {
+    var guess = event.key;
+    if (guess === theChosenLetter) {
+        win();
+    } else if (remainingGuess - 1 === 0) {
+        lose();
+    } else {
+        fail(guess);
+    }
+
+    display();
+}
+
+function display() {
+    var winNumber = document.getElementById("win");
+    var lossNumber = document.getElementById("loss");
+    var guessRemaining = document.getElementById("remaining");
+    var letterTyped = document.getElementById("guessed")
+    winNumber.innerHTML = win;
+    lossNumber.innerHTML = loss;
+    guessRemaining.innerHTML = remainingGuess;
+    letterTyped.innerHTML = guessedLetter.join(',');
+}
+
+
+function win() {
+    win++;
+    resetGame();
+}
+
+function lose() {
+    loss++;
+    resetGame();
+}
+
+function fail(letter) {
+    remainingGuess--;
+    guessedLetter.push(letter);
+}
+
+function resetGame() {
+    guessRemaining = 10;
+    guessedLetter = [];
     theChosenLetter =  letters[Math.floor(Math.random() * letters.length)];
-    winCount = 0;
-    lossCount = 0;
+
+    console.log(" Random letter" + theChosenLetter);
     
 }
 
-var userText = document.getElementById("user-text");
 
-document.onkeyup = function(event) {
-    userText.textContent = event.key;
-};
-
-console.log(theChosenLetter);
